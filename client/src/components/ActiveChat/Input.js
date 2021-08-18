16713654da69d5238dd21f25c7741b7850b55af9
Rohan Batra <rohanbatra24@gmail.com>
2021-08-17 21:07:33 -0400

    create isNewConvo property

--- client/src/components/ActiveChat/Input.js
+++ client/src/components/ActiveChat/Input.js
@@ -7,14 +7,14 @@ import { postMessage } from "../../store/utils/thunkCreators";
 const useStyles = makeStyles(() => ({
   root: {
     justifySelf: "flex-end",
-    marginTop: 15
+    marginTop: 15,
   },
   input: {
     height: 70,
     backgroundColor: "#F4F6FA",
     borderRadius: 8,
-    marginBottom: 20
-  }
+    marginBottom: 20,
+  },
 }));
 
 const Input = (props) => {
@@ -31,9 +31,9 @@ const Input = (props) => {
     // add sender user info if posting to a brand new convo, so that the other user will have access to username, profile pic, etc.
     const reqBody = {
       text: event.target.text.value,
-      recipientId: otherUser.id,
-      conversationId,
-      sender: conversationId ? null : user
+      recipient: otherUser,
+      isNewConvo: !conversationId ? true : false,
+      sender: user,
     };
     await postMessage(reqBody);
     setText("");
