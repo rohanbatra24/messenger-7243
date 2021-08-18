6324237bae397e8bcd58ebe6da00931f773dd530
Rohan Batra <rohanbatra24@gmail.com>
2021-08-18 11:23:33 -0400

    diaplay messages by newest first

--- client/src/components/ActiveChat/Messages.js
+++ client/src/components/ActiveChat/Messages.js
@@ -6,15 +6,24 @@ import moment from "moment";
 const Messages = (props) => {
   const { messages, otherUser, userId } = props;
 
+  const sortedMessagesByTimeAsc = messages.sort(
+    (a, b) => moment(a.createdAt) - moment(b.createdAt)
+  );
+
   return (
     <Box>
-      {messages.map((message) => {
+      {sortedMessagesByTimeAsc.map((message) => {
         const time = moment(message.createdAt).format("h:mm");
 
         return message.senderId === userId ? (
           <SenderBubble key={message.id} text={message.text} time={time} />
         ) : (
-          <OtherUserBubble key={message.id} text={message.text} time={time} otherUser={otherUser} />
+          <OtherUserBubble
+            key={message.id}
+            text={message.text}
+            time={time}
+            otherUser={otherUser}
+          />
         );
       })}
     </Box>
