8535836da7d9a038824b72de4db56696ecfccc24
Rohan Batra <rohanbatra24@gmail.com>
2021-08-18 08:58:14 -0400

    revert previous changes

--- client/src/components/ActiveChat/Input.js
+++ client/src/components/ActiveChat/Input.js
@@ -31,9 +31,9 @@ const Input = (props) => {
     // add sender user info if posting to a brand new convo, so that the other user will have access to username, profile pic, etc.
     const reqBody = {
       text: event.target.text.value,
-      recipient: otherUser,
-      isNewConvo: !conversationId ? true : false,
-      sender: user,
+      recipientId: otherUser.id,
+      conversationId,
+      sender: conversationId ? null : user,
     };
     await postMessage(reqBody);
     setText("");
