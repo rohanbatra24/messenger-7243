4d6247cd447b43ade65f079f094a381e27a0f0c4
Rohan Batra <rohanbatra24@gmail.com>
2021-08-20 20:55:41 -0400

    emit 'message seen' when active chat clicked

--- client/src/components/Sidebar/Chat.js
+++ client/src/components/Sidebar/Chat.js
@@ -1,9 +1,14 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Box } from "@material-ui/core";
 import { BadgeAvatar, ChatContent } from "../Sidebar";
 import { makeStyles } from "@material-ui/core/styles";
 import { setActiveChat } from "../../store/activeConversation";
 import { connect } from "react-redux";
+import socket from "../../socket";
+import {
+  updateConvoLastOpened,
+  updateReadByMe,
+} from "../../store/conversations";
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -14,19 +19,49 @@ const useStyles = makeStyles((theme) => ({
     display: "flex",
     alignItems: "center",
     "&:hover": {
-      cursor: "grab"
-    }
-  }
+      cursor: "grab",
+    },
+  },
 }));
+
+const unreadMessagesStyles = {
+  backgroundColor: "#3D91FF",
+  color: "white",
+  marginRight: "1rem",
+  width: "1.3rem",
+  textAlign: "center",
+  borderRadius: "50%",
+};
 
 const Chat = (props) => {
   const classes = useStyles();
-  const { conversation } = props;
+  const { conversation, activeConversation } = props;
   const { otherUser } = conversation;
 
   const handleClick = async (conversation) => {
     await props.setActiveChat(conversation.otherUser.username);
+
+    if (conversation.id) {
+      // Emit message to notify that all messages in this conversation are seen
+      socket.emit("messages-seen", {
+        conversationId: conversation.id,
+      });
+
+      // Update store with the time this convo was last opened
+      props.updateConvoLastOpened(conversation);
+      props.updateReadByMe(conversation.id);
+    }
   };
+
+  const lastMessage = conversation.id
+    ? conversation.messages[conversation.messages.length - 1].id
+    : null;
+
+  useEffect(() => {
+    if (activeConversation === conversation.otherUser.username) {
+      props.updateReadByMe(conversation.id);
+    }
+  }, [activeConversation, lastMessage]);
 
   return (
     <Box onClick={() => handleClick(conversation)} className={classes.root}>
@@ -37,6 +72,11 @@ const Chat = (props) => {
         sidebar={true}
       />
       <ChatContent conversation={conversation} />
+      {conversation.unreadByMe ? (
+        <span style={unreadMessagesStyles}>{conversation.unreadByMe}</span>
+      ) : (
+        ""
+      )}
     </Box>
   );
 };
@@ -45,8 +85,20 @@ const mapDispatchToProps = (dispatch) => {
   return {
     setActiveChat: (id) => {
       dispatch(setActiveChat(id));
-    }
+    },
+    updateConvoLastOpened: (conversation) => {
+      dispatch(updateConvoLastOpened(conversation));
+    },
+    updateReadByMe: (conversationId) => {
+      dispatch(updateReadByMe(conversationId));
+    },
   };
 };
 
-export default connect(null, mapDispatchToProps)(Chat);
+const mapStateToProps = (state) => {
+  return {
+    activeConversation: state.activeConversation,
+  };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(Chat);
