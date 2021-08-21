25e73263f39ae9977ae338b87d215813dcc8cd42
Rohan Batra <rohanbatra24@gmail.com>
2021-08-20 20:54:20 -0400

    show badge avatar on latest seen message

--- client/src/components/ActiveChat/Messages.js
+++ client/src/components/ActiveChat/Messages.js
@@ -1,10 +1,12 @@
-import React from "react";
-import { Box } from "@material-ui/core";
-import { SenderBubble, OtherUserBubble } from "../ActiveChat";
+import React, { Fragment } from "react";
 import moment from "moment";
+import { Box } from "@material-ui/core";
+
+import { SenderBubble, OtherUserBubble } from "../ActiveChat";
+import { BadgeAvatar } from "../Sidebar";
 
 const Messages = (props) => {
-  const { messages, otherUser, userId } = props;
+  const { messages, otherUser, userId, lastMessageIdSeenByOtherUser } = props;
 
   const sortedMessagesByTimeAsc = messages.sort((a, b) => {
     return new Date(a.createdAt).getTime() - new Date(b.createdAt).getTime();
@@ -16,14 +18,36 @@ const Messages = (props) => {
         const time = moment(message.createdAt).format("h:mm");
 
         return message.senderId === userId ? (
-          <SenderBubble key={message.id} text={message.text} time={time} />
+          <Fragment>
+            <SenderBubble key={message.id} text={message.text} time={time} />
+            {lastMessageIdSeenByOtherUser === message.id && (
+              <BadgeAvatar
+                photoUrl={otherUser.photoUrl}
+                username={otherUser.username}
+                online={otherUser.online}
+                sidebar={true}
+                alignRight={true}
+              />
+            )}
+          </Fragment>
         ) : (
-          <OtherUserBubble
-            key={message.id}
-            text={message.text}
-            time={time}
-            otherUser={otherUser}
-          />
+          <Fragment>
+            <OtherUserBubble
+              key={message.id}
+              text={message.text}
+              time={time}
+              otherUser={otherUser}
+            />
+            {lastMessageIdSeenByOtherUser === message.id && (
+              <BadgeAvatar
+                photoUrl={otherUser.photoUrl}
+                username={otherUser.username}
+                online={otherUser.online}
+                sidebar={true}
+                alignRight={true}
+              />
+            )}
+          </Fragment>
         );
       })}
     </Box>
