0c9002eb6c0ade9a099e9247c4ac085122a3f684
Rohan Batra <rohanbatra24@gmail.com>
2021-08-26 16:11:32 -0400

    update findConversations method to get all convos where sender & recipient are participants

--- server/db/models/conversation.js
+++ server/db/models/conversation.js
@@ -1,6 +1,8 @@
 const db = require("../db");
 const Message = require("./message");
+const { Op } = require("sequelize");
 const Sequelize = require("sequelize");
+
 const User = require("./user");
 
 const Conversation = db.define("conversation", {
@@ -10,15 +12,15 @@ const Conversation = db.define("conversation", {
   },
 });
 
-// find conversation given an array of user Ids
+// find conversations given two user Ids
 
-Conversation.findConversations = async function (userIdsArray) {
+Conversation.findConversations = async function (senderId, recipientId) {
   const conversation = await Conversation.findAll({
     attributes: ["id"],
     order: [[Message, "createdAt", "DESC"]],
     include: [
       { model: Message, order: ["createdAt", "DESC"] },
-      { model: User, where: { id: userIdsArray } },
+      { model: User, where: { id: [senderId, recipientId] } },
     ],
   });
 
